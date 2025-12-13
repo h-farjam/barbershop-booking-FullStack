9b6c0c2f3089f5a553e8a9a34201b310b47533de
h_farjam <hosseinfarjamfar@gmail.com>
2025-12-13 14:39:40 +0330

    Edit import Servises data

--- src/app/api/importServices/route.ts
+++ src/app/api/importServices/route.ts
@@ -3,32 +3,31 @@ import { NextResponse } from "next/server";
 import ConnectDB from "@/utils/ConnectDB";
 import Service from "@/models/Service";
 
-
 const servicesData = [
   {
     title: "اصلاح صورت",
     subtitle: "اصلاح حرفه ای صورت با جدید ترین روش ها",
-    img: "https://cdn11.bigcommerce.com/s-h7l2pcerei/product_images/uploaded_ima…",
-    price: "100 هزار",
+    img: "https://example.com/face.jpg",
+    price: 100,
   },
   {
     title: "پاکسازی پوست",
     subtitle: "پاکسازی با بهترین مواد داخل بازار",
-    img: "https://i-media.vyaparify.com/vcards/products/10374/images-(54).jpeg",
-    price: "700 هزار",
+    img: "https://example.com/skin.jpg",
+    price: 700,
   },
   {
     title: "پکیج دامادی VIP",
     subtitle: "پاکسازی با بهترین مواد داخل بازار",
-    img: "https://damadbarber.com/wp-content/uploads/2020/06/img_3989-833x1024.j…",
-    price: "3 میلیون",
+    img: "https://example.com/vip.jpg",
+    price: 3,
   },
-  
+
   {
     title: "اصلاح کامل سر و صورت",
     subtitle: "اصلاح کامل همراه باحالت دهی",
-    img: "https://cafeegraphic.com/wp-content/uploads/edd/2025/07/Barbershop-pho…",
-    price: "230 هزار",
+    img: "https://example.com/full.jpg",
+    price: 230,
   },
 ];
 
@@ -42,7 +41,10 @@ export async function POST() {
     // درج همه سرویس‌ها
     await Service.insertMany(servicesData);
 
-    return NextResponse.json({ message: "Services imported!" }, { status: 200 });
+    return NextResponse.json(
+      { message: "Services imported!" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Import Services Error:", error);
     return NextResponse.json(
